c4a7a9a80d67623418a1e0ea95fa583b6c6d732d
Oscar <oscar.ramos@ucsp.edu.pe>
2020-09-11 23:46:21 -0500

    feat: personal styles

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ const useStyles = makeStyles(theme => ({
     borderRadius: 10,
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     boxShadow: '0px 10px 10px 10px rgba(0, 0, 0, 0.2)',
+    backdropFilter: 'blur(0.1rem)',
     [theme.breakpoints.down('md')]: {
       margin: '0 10px',
     },
